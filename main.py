345eca005b6b759360167fd3ff37ee05e59b9c7e
axelptc <96388731+axelptc@users.noreply.github.com>
2022-10-04 19:53:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pytz import timezone
 
 def sondage(context:CallbackContext):
     options = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche','Pas là', 'Sydney Govou']
-    bot.send_poll(-1001165380533, 'Five cette semaine', options, False, allows_multiple_answers=True )
+    bot.send_poll(1953752060, 'Five cette semaine', options, False, allows_multiple_answers=True )
 
 
 updater = Updater('5672453377:AAG-MbOf4Ni-yIDCYV1he8C0XHlpxMQX7cw', use_context=True)
