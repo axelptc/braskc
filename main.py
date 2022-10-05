e22516c5f0127ed165ba4b62bc054aa0a145b344
axelptc <96388731+axelptc@users.noreply.github.com>
2022-10-05 20:50:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def sondage(context:CallbackContext):
     options = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche','Pas là', 'Sydney Govou']
     bot.send_poll(1953752060, 'Five cette semaine', options, False, allows_multiple_answers=True )
 
-
 updater = Updater('5672453377:AAG-MbOf4Ni-yIDCYV1he8C0XHlpxMQX7cw', use_context=True)
 bot = updater.bot
 j = updater.job_queue
