b219fdfba0022739a09530127b1dba5ce8a1ce57
axelptc <96388731+axelptc@users.noreply.github.com>
2022-10-06 16:46:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from telegram import*
 from telegram.ext import*
 from datetime import*
@@ -7,9 +8,10 @@ def sondage(context:CallbackContext):
     options = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche','Pas là', 'Sydney Govou']
     bot.send_poll(1953752060, 'Five cette semaine', options, False, allows_multiple_answers=True )
 
-updater = Updater('5672453377:AAG-MbOf4Ni-yIDCYV1he8C0XHlpxMQX7cw', use_context=True)
+updater = Updater('5672453377:AAER_0xzOwUs8hVcKyEb_l-xcte6Y_16oVg', use_context=True)
 bot = updater.bot
 j = updater.job_queue
-j.run_daily(sondage, time=time(11,33,0,0),days=(0,1,2,3,4,5,6))
+j.run_daily(sondage, time=time(14,50,0,0),days=Tuple(4))
+updater.start_polling()
 
 
