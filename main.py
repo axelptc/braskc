c3ef187f7f49c1937463b55ade97777fcec1a7ca
axelptc <96388731+axelptc@users.noreply.github.com>
2022-10-11 15:17:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ def sondage(context:CallbackContext):
     options = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche','Pas là', 'Sidney Govou']
     bot.send_poll(-1001165380533, 'Five cette semaine', options, False, allows_multiple_answers=True )
 
-def test(context:CallbackContext):
-    bot.send_message(1953752060,'bang')
+def heure(context:CallbackContext):
+    options = ['10h', '11h', '12h', '13h', '14h', '15h', '16h','17h', 'Sidney Govou']
+    bot.send_poll(-1001165380533, 'Horaire Samedi', options, False, allows_multiple_answers=True )
 
 updater = Updater('5672453377:AAER_0xzOwUs8hVcKyEb_l-xcte6Y_16oVg', use_context=True)
 bot = updater.bot
 j = updater.job_queue
 j.run_repeating(sondage, timedelta(days=7), first=datetime(2022,10,9,14,0,0))
-j.run_repeating(test, timedelta(days=1), first=datetime(2022,10,8,7,0,0))
+j.run_repeating(heure, timedelta(days=7), first=datetime(2022,10,12,16,0,0))
+j.run_repeating(heure, timedelta(days=7), first=datetime(2022,10,8,7,0,0))
 updater.start_polling()
 updater.idle()
